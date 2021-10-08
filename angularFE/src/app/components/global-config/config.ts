8af87a60b95ec954c50d2e794333c7ad11e3a8e2
rmunoz <richosojason@msn.com>
2021-10-07 22:50:52 -0500

    code formatted

--- angularFE/src/app/components/global-config/config.ts
+++ angularFE/src/app/components/global-config/config.ts
@@ -1,21 +1,19 @@
 export class Config {
-
   localhost: string;
 
   /* constructor(localhost) {
     this.localhost = localhost;
   } */
 
-  constructor(){
-    this.localhost = 'http://192.168.100.106:3000/';
+  constructor() {
+    this.localhost = "http://192.168.100.106:3000/";
   }
 
   getLocalhostURI() {
     return this.localhost;
   }
 
-  setLocalhostURI(localhost) {
+  setLocalhostURI(localhost: string) {
     this.localhost = localhost;
   }
-
 }
